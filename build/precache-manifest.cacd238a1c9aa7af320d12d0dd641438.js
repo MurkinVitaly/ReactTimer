e6bdc6f565273ecf465621542b379e6b2404f841
Vitaliy <69913619+MurkinVitaly@users.noreply.github.com>
2022-01-22 01:48:18 +0200

    first commit

---
+++ build/precache-manifest.cacd238a1c9aa7af320d12d0dd641438.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8d0315b7a6cfd8e1fbe",
+    "url": "/MurkinVitaly/MurkinVitaly.github.io/static/css/main.de05742f.chunk.css"
+  },
+  {
+    "revision": "a8d0315b7a6cfd8e1fbe",
+    "url": "/MurkinVitaly/MurkinVitaly.github.io/static/js/main.a8d0315b.chunk.js"
+  },
+  {
+    "revision": "7f7fbd61c36ac064ac9b",
+    "url": "/MurkinVitaly/MurkinVitaly.github.io/static/js/1.7f7fbd61.chunk.js"
+  },
+  {
+    "revision": "e9550590caac499c1ef7",
+    "url": "/MurkinVitaly/MurkinVitaly.github.io/static/js/2.e9550590.chunk.js"
+  },
+  {
+    "revision": "96e07c13ca23c80cb5e3",
+    "url": "/MurkinVitaly/MurkinVitaly.github.io/static/js/runtime~main.96e07c13.js"
+  },
+  {
+    "revision": "b1fba15f5cfbebd1a373ae233d38d555",
+    "url": "/MurkinVitaly/MurkinVitaly.github.io/static/media/time.b1fba15f.jpg"
+  },
+  {
+    "revision": "4dd59e1b3d2e2086e9f6c5dd111f1c51",
+    "url": "/MurkinVitaly/MurkinVitaly.github.io/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,68 +1,65 @@
-import React, { useState, useRef } from 'react';
-import "./App.css"
+import { render } from "@testing-library/react";
+import React, { useState, useRef } from "react";
+import "./App.css";
+import imageName from "./image/time.jpg";
 
 function padTime(time) {
-    return time.toString().padStart(2, '0');
+  return time.toString().padStart(2, "0");
 }
 export default function App() {
-    const [title, setTitle] = useState('Let the countdown begin!!!')
-    const [timeLeft, setTimeLeft] = useState(25 * 60);
-    const [isRunning, setIsRunning] = useState(false);
-    const intervalRef = useRef(null);
+  const [title, setTitle] = useState("Let the countdown begin!!!");
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
 
-    function startTimer() {
-        setTitle('You are doing great');
-        if (intervalRef.current !== null) return;
-        setIsRunning(true);
-        intervalRef.current = setInterval(() => {
-            setTimeLeft(timeLeft => {
-                if (timeLeft >= 1) return timeLeft - 1;
+  function startTimer() {
+    setTitle("You are doing great");
+    if (intervalRef.current !== null) return;
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        if (timeLeft >= 1) return timeLeft - 1;
 
-                // reset the timer
-                return 0;
-            });
-        }, 1000);
-    }
+        // reset the timer
+        return 0;
+      });
+    }, 1000);
+  }
+  console.log(intervalRef.current);
 
-    console.log(intervalRef.current);
+  function stopTimer() {
+    setTitle("Keep it up");
+    if (intervalRef.current === null) return;
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setIsRunning(false);
+  }
 
-    function stopTimer() {
-        setTitle('Keep it up');
-        if (intervalRef.current === null) return;
-        clearInterval(intervalRef.current);
-        intervalRef.current = null;
-        setIsRunning(false);
-    }
+  function resetTimer() {
+    setTitle("Ready to go another round?");
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTimeLeft(25 * 60);
+    setIsRunning(false);
+  }
 
-    function resetTimer() {
-        setTitle('Ready to go another round?')
-        clearInterval(intervalRef.current);
-        intervalRef.current = null;
-        setTimeLeft(25 * 60);
-        setIsRunning(false);
-    }
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+  return (
+    <div className="app">
+      <h2>{title}</h2>
 
-    const minutes = padTime(Math.floor(timeLeft / 60));
-    const seconds = padTime(timeLeft - minutes * 60);
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
 
-    return (
-        <div className='app'>
-            <h2>{title}</h2>
-
-
-            <div className='timer'>
-                <span>{minutes}</span>
-                <span>:</span>
-                <span>{seconds}</span>
-            </div>
-
-            <div className='buttons'>
-            {!isRunning && <button onClick={startTimer}>Start</button>}
-            {isRunning && <button onClick={stopTimer}>Stop</button>}
-            <button onClick={resetTimer}>Reset</button>
-            </div>
-        </div>
-    );
+      <div className="buttons">
+        {!isRunning && <button onClick={startTimer}>Start</button>}
+        {isRunning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
+      </div>
+    </div>
+  );
 }
-
-
